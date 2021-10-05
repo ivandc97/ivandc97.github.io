a8d8fc7a325a24ea820bfced2628c01e5668ee25
i.halahdin <i.halahdin@ideil.com>
2021-10-05 11:03:17 +0300

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-main-wrap />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import VMainWrap from './components/VMainWrap';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {VMainWrap},
+  methods: {}
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
